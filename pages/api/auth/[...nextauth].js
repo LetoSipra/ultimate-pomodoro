d7f0f2eb43370b88c8e97858be1907c8545f442c
LetoSipra(Yusuf) <yusufakcay22.0@gmail.com>
2023-04-10 16:51:56 +0300

    secretAdded

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, token }) {
       session.user.uid = token.sub;
